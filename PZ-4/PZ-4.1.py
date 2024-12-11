da8e7f97f007f04f885e83524fbaff408cf148a7
kolastukov <kolasystinov@gmail.com>
2024-12-11 15:50:32 +0300

    UPGRADE

---
+++ PZ-4/PZ-4.1.py
@@ -0,0 +1,9 @@
+#Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1.2, 1.4, ..., 2 кг
+#конфет.
+
+price = int(input("Введите цену за 1 кг конфет: "))
+weight = 1.0
+while weight <= 2:
+    print(price * weight)
+    weight += 0.2
+    print(weight)
