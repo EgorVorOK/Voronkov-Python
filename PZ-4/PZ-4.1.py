57dabc94dd2db799442132a00e1f5f7e98d7877d
kolastukov <kolasystinov@gmail.com>
2024-12-13 16:14:54 +0300

    UPGRADE

--- PZ-4/PZ-4.1.py
+++ PZ-4/PZ-4.1.py
@@ -1,8 +1,10 @@
 #Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1.2, 1.4, ..., 2 кг
 #конфет.
-
-price = int(input("Введите цену за 1 кг конфет: "))
-weight = 1.0
-while weight <= 2:
-    print("Цена за ", weight, "равна ",  price * weight)
-    weight += 0.2
+try:
+    price = int(input("Введите цену за 1 кг конфет: "))
+    weight = 1.0
+    while weight <= 2:
+        print("Цена за ", weight, "равна ",  price * weight)
+        weight += 0.2
+except ValueError:
+    print("ОШИБКА!")
--- PZ-4/PZ-4.2.py
+++ PZ-4/PZ-4.2.py
@@ -1,3 +1,25 @@
-# Дано целое число N (> 0). Используя операции деления нацело и взятия остатка от
-# деления, найти число, полученное при прочтении числа N справа налево.
+#Дано целое число N (>0). Используя операции деления нацело и взятия остатка от
+#деления, найти количество и сумму его цифр.
 
+def count_and_sum_digits(n):
+    count = 0
+    total_sum = 0
+
+    while n > 0:
+        last_digit = n % 10
+        count += 1
+        total_sum += last_digit
+        n //= 10
+    return count, total_sum
+
+try:
+    n = int(input("Введите целое число N (> 0): "))
+    if n > 0:
+        print(count_and_sum_digits(n))
+        digit_count, digit_sum = count_and_sum_digits(n)
+        print(f"Количество цифр: {digit_count}")
+        print(f"Сумма цифр: {digit_sum}")
+    else:
+        print("Число должно быть больше 0.")
+except ValueError:
+    print("ОШИБКА!")
