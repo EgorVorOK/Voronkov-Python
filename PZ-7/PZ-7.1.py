38a5cba43014f4bb1b78bdd4774d5c66f5277031
EgorVorOK <vev891@gmail.com>
2025-01-27 19:36:35 +0300

    UPgrade

---
+++ PZ-7/PZ-7.1.py
@@ -0,0 +1,14 @@
+#Дано целое число N (1 < N < 26). Вывести N последних строчных (то есть маленьких)
+#букв латинского алфавита в обратном порядке (начиная с буквы «z»).
+
+try:
+    sp = "abcdefghijklmnopqrstuvwxyz"
+    n = int(input("Введите число, выполняющее условие: 1 < N < 26 "))
+    if n < 1 or n > 26:
+        raise ValueError
+    new_sp = sp[::-1]
+    print((new_sp[:n]))
+except ValueError:
+    print("ОШИБКА!")
+
+
